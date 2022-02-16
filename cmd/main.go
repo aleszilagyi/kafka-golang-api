8c71eddf5d2ca19beb050c41d2c6c9cea92edf35
Alexandre Szilagyi Santos <alexandre.szilagyi.santoz@gmail.com>
2022-02-16 17:37:10 -0400

    (chore) add env config file with viper and auto creation of topic courses

--- cmd/main.go
+++ cmd/main.go
@@ -3,19 +3,38 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 
-	courseUsecase "github.com/aleszilagyi/kafka-golang-api/domain/usecase"
-	courseRepository "github.com/aleszilagyi/kafka-golang-api/adapters/repository"
-	kafkaCourse "github.com/aleszilagyi/kafka-golang-api/adapters/kafka"
 	logWrapper "github.com/aleszilagyi/kafka-golang-api/adapters/config/logger"
+	kafkaCourse "github.com/aleszilagyi/kafka-golang-api/adapters/kafka"
+	courseRepository "github.com/aleszilagyi/kafka-golang-api/adapters/repository"
+	courseUsecase "github.com/aleszilagyi/kafka-golang-api/domain/usecase"
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
+	viper "github.com/spf13/viper"
 )
 
 func main() {
 	log := logWrapper.NewLogger()
 
-	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/courses")
+	viper.SetConfigName("application")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	dbConfigs := viper.GetStringMapString("application.mysql")
+	dbString := fmt.Sprintf("%s:%s@tcp(mysql:%s)/%s", dbConfigs["user"], dbConfigs["password"], dbConfigs["port"], dbConfigs["table"])
+
+	configMapConsumer := &ckafka.ConfigMap{}
+
+	topics := viper.GetStringSlice("application.kafka.topics")
+	viper.UnmarshalKey("application.kafka.configs", configMapConsumer)
+
+	db, err := sql.Open("mysql", dbString)
 	if err != nil {
 		log.Error(err)
 	}
@@ -23,12 +42,7 @@ func main() {
 	usecase := courseUsecase.CreateCourse{Repository: repository}
 
 	var messageChannel = make(chan *ckafka.Message)
-	configMapConsumer := &ckafka.ConfigMap{
-		"bootstrap.servers": "kafka:9092",
-		"client.id":         "kafka-course",
-		"group.id":          "kafka-course",
-	}
-	topics := []string{"courses"}
+
 	consumer := kafkaCourse.NewConsumer(configMapConsumer, topics)
 	go consumer.Consume(messageChannel)
 
